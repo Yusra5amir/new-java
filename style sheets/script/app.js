1d8c3626a728a1eb811cc05c79ca1c8f77f746f4
YUSRA_AMIR <muhammadibaad134@gmail.com>
2023-04-08 20:23:44 +0500

    Java 1st Class

---
+++ style sheets/script/app.js
@@ -0,0 +1,43 @@
+// <!---4 april---->
+// This is my first javascript code!gi
+// alert('Hello');
+// prompt('Your Name Please','Yusra')
+// var arr=('Hello','Hi')
+//  console.log(arr);
+//  var arr=('Hello Pakistan');
+//  console.log(arr);
+//  console.assert(hello);
+//  var alert=('main thek hon');
+//  console.log("3"+"5");
+//  console.log(2+4);
+//  var arr=("num")
+
+//  var arr= [ 'Hello' , 'Hi']
+// document.write(arr)
+// console.log(arr)
+ 
+//  <....6 april....>
+//  console.log('Hello everyone');
+//  let name='YusraAmir';
+//  console.log(name);
+//  alert("Hello");
+//  prompt('Your Name Please','Yusra');
+//  var arr=('Hello','Hi')
+//  console.log(arr);
+ 
+ // Cannot be a reserved keyword
+ // Should be meaningful
+ // Cannot start with a number (2name)
+ // Cannot cannot a space or hyphen(-)
+ //Are case-sensitive
+
+
+//  let firstname = 'Yusra';
+//  let fname = 'Amir'
+
+//  <!---7-april--->
+alert ("Zakat Calculator");
+var zakatPercentage = 0.025;
+var userInput = (prompt("Enter Your Amount"));
+var result = (zakatPercentage) * (userInput);
+alert ("Your Zakat Value is" + result);
